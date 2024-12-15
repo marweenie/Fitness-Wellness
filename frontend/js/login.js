5e40b56fb985ea1486cf3b9b8900e01a94aab45a
marweenie <133703779+marweenie@users.noreply.github.com>
2024-12-14 19:33:11 -0500

    HTML/JS split into 2 files from login

---
+++ frontend/js/login.js
@@ -0,0 +1,29 @@
+document.getElementById('loginForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    var formData = new FormData(this);
+
+    fetch('../backend/login.php', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.message && data.message === 'Login successful') {
+            document.getElementById('message').innerText = data.message;
+            document.getElementById('userInfo').style.display = 'block';
+            document.getElementById('userID').innerText = data.user.UserID;
+            document.getElementById('userName').innerText = data.user.Name;
+            document.getElementById('userAge').innerText = data.user.Age;
+            document.getElementById('userGender').innerText = data.userGender;
+            document.getElementById('userHeight').innerText = data.user.Height;
+            document.getElementById('userWeight').innerText = data.user.Weight;
+        } else {
+            document.getElementById('message').innerHTML = "Error: " + data.error;
+            document.getElementById('userInfo').style.display = 'none';
+        }
+    })
+    .catch(error => {
+        document.getElementById('message').innerHTML = "Fetch error: " + error;
+    });
+});
